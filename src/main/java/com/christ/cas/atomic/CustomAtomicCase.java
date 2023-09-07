073ee0d780fbb70e81a58300212cae5c621677ad
sxc <xiecheng.shi@raipiot.com>
2023-09-07 16:45:02 +0800

    引用类型markable case

--- src/main/java/com/christ/cas/atomic/CustomAtomicCase.java
+++ src/main/java/com/christ/cas/atomic/CustomAtomicCase.java
@@ -3,7 +3,9 @@ package com.christ.cas.atomic;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 /**
  * @author 史偕成
@@ -42,6 +44,39 @@ class CustomIntegerCase {
         }
         countDownLatch.await();
         log.info("最终计算结果：{}", customInteger.number);
+    }
+}
+
+@Slf4j
+class CustomAtomicMarkableReferenceCase {
+
+    static AtomicMarkableReference<Integer> data = new AtomicMarkableReference<Integer>(100, false);
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            boolean marked = data.isMarked();
+            log.info("获取标识位：{}", marked);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            data.compareAndSet(100, 101, marked, !marked);
+        }, "t1").start();
+
+        new Thread(() -> {
+            boolean marked = data.isMarked();
+            log.info("获取标识位：{}", marked);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            boolean b = data.compareAndSet(101, 2000, marked, !marked);
+
+            log.info("CAS result: {}, marked: {}, data:{}", b, data.isMarked(), data.getReference());
+
+        }, "t2").start();
     }
 }
 
